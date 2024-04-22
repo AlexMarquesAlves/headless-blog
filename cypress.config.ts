fd4adbc5cc80bad943a996c1bacc50bfbbdbb23e
¡Duque <atanaelleonardo@gmail.com>
2024-04-22 15:01:10 -0300

    Update .gitignore and add Cypress configuration scripts
    * Added Cypress-related directories to .gitignore
    * Added scripts for running Cypress tests in various configurations in package.json

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    // baseUrl: 'http://localhost:3000',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
+    experimentalRunAllSpecs: true,
+  },
+  fixturesFolder: false,
+  video: true,
+  viewportWidth: 1280,
+  viewportHeight: 880,
+})
