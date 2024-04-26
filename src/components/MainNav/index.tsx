2025ffd3b4ae00758276e78ff6a2cd63cd11fe71
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 02:14:02 -0300

    "Add site name and logo to MainNav"
    This commit adds the site configuration name and logo to the MainNav component using the Next.js Link component and the Icons.logo component. The nav element has also been updated with a

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { siteConfig } from '@/config/site'
+import Link from 'next/link'
 import { ReactNode } from 'react'
+import { Icons } from '../Icons'
 
 interface MainNavProps {
   children?: ReactNode
@@ -9,7 +12,12 @@ interface MainNavProps {
 export function MainNav({ children }: MainNavProps) {
   return (
     <>
-      <nav>MainNav</nav>
+      <nav className="flex items-center space-x-4 lg:space-x-6">
+        <Link href={`/`} className="flex items-center mr-6 space-x-2">
+          <Icons.logo className="h-6 w-6" />
+          <span className="font-bold">{siteConfig.name}</span>
+        </Link>
+      </nav>
       {children}
     </>
   )
