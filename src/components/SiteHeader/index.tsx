d092ea8b9c263f02d05232df26edb3a6fb870c50
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 01:08:24 -0300

    "Update SiteHeader component with site configuration, utility classes, and GitHub link"
    This commit updates the `SiteHeader` component by incorporating the site configuration, utility classes, and a link to the project's GitHub repository.

--- src/components/SiteHeader/index.tsx
+++ src/components/SiteHeader/index.tsx
@@ -1,14 +1,28 @@
-import { ReactNode } from 'react'
+import { siteConfig } from '@/config/site'
+import { cn } from '@/lib/utils'
+import { Link } from 'lucide-react'
+import { Icons } from '../Icons'
+import { buttonVariants } from '../ui/button'
 
-interface SiteHeaderProps {
-  children?: ReactNode
-}
-
-export function SiteHeader({ children }: SiteHeaderProps) {
+export function SiteHeader() {
   return (
-    <>
-      <h1>SiteHeader</h1>
-      {children}
-    </>
+    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <div className="flex flex-1 items-center justify-end space-x-2">
+          <nav className="flex items-center">
+            <Link href={siteConfig.links.github} target="_blank">
+              <div
+                className={cn(
+                  buttonVariants({ variant: 'ghost' }),
+                  'w-10 px-0',
+                )}
+              >
+                <Icons.gitHub className="h-4 w-4" />
+              </div>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
   )
 }
