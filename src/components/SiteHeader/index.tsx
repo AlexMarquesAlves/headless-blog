3b33a1c36321fac9c0a7141d02306e17548e4fa7
¡Duque <atanaelleonardo@gmail.com>
2024-04-26 02:08:35 -0300

    "Add MainNav component to SiteHeader"
    This commit adds the MainNav component to the SiteHeader component. The MainNav component is responsible for rendering the main navigation links of the website.

--- src/components/SiteHeader/index.tsx
+++ src/components/SiteHeader/index.tsx
@@ -2,12 +2,14 @@ import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { Icons } from '../Icons'
+import { MainNav } from '../MainNav'
 import { buttonVariants } from '../ui/button'
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
+        <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center">
             <Link
